a695b40169e3505399d80f79c11cf6eb6e0213c7
Abdulraheman <abdulrahemanshaikh2006@gmail.com>
2025-10-25 11:47:21 +0200

    third commit

--- 1_introduction_exercises2.py
+++ 1_introduction_exercises2.py
@@ -1,3 +1,9 @@
 #Write a program that takes your name as input and prints a personalized greeting along with a calculation of your birth year.
-
+# personalized greeting along with birth year calculation   
+#input
 Name = input("Enter your name: ")
+print("Hello, " + Name + "!")
+age = int(input("Enter your age: "))
+current_year = 2025
+birth_year = current_year - age
+print("You were born in the year " + str(birth_year) + ".")
