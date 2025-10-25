3952fa1721695add364d2a5204c15303561c2db4
Abdulraheman <abdulrahemanshaikh2006@gmail.com>
2025-10-25 12:40:55 +0200

    fifth commit

---
+++ 2_dataTypes_floatingPoint.py
@@ -0,0 +1,8 @@
+Value1 = 0.1
+Value2 = 0.2    
+total = Value1 + Value2
+print("The Total is:", total)
+if total == 0.3:
+    print("The total is exactly 0.3")
+if total != 0.3:
+    print("The total is not exactly 0.3")
